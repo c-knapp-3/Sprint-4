af13cbd7bdcab65f21a7af17e4259014da228525
Carolyn Knapp <113488299+c-knapp-3@users.noreply.github.com>
2024-04-14 17:20:02 -0500

    Create TestComputer.java

---
+++ test/TestComputer.java
@@ -0,0 +1,17 @@
+package sprint4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sprint4.Board;
+import sprint4.Board.Cell;
+import sprint4.Computer;
+import sprint4.SimpleGame;
+
+class TestComputer {
+	private Computer computerPlayer;
+	private Board board;
+
